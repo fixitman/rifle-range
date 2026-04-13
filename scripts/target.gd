5b1c83c91c6a58e3261c0a20b91662950689a522
Mike <fixitman.mike@gmail.com>
2026-04-13 16:52:42 -0400

    new target

--- scripts/target.gd
+++ scripts/target.gd
@@ -3,6 +3,7 @@ extends Node3D
 var triggered:bool = false
 var height:float = 2.0
 const RESET_ROTATION := 90.0
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,11 +17,14 @@ func _process(delta: float) -> void:
 func trigger() -> void:
 	if !triggered:
 		#%HitBox.global_translate(Vector3(0.0,height,0.0))
-		%HitBox.global_rotate(Vector3.UP,PI/2)
+		#%HitBox.global_rotate(Vector3.UP,-PI/2)
+		animation_player.play("trigger")
 		triggered = true
 		
 func reset() -> void:
 	if triggered:
-		%HitBox.global_translate(Vector3(0.0,-height,0.0))
+		#%HitBox.global_translate(Vector3(0.0,-height,0.0))
+		#%HitBox.global_rotate(Vector3.UP,PI/2)
+		animation_player.play("reset")
 		triggered = false
 	
