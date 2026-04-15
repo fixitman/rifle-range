0b3d97d3b79b82c14d2a9f95fdae5de051867762
Mike <fixitman.mike@gmail.com>
2026-04-15 16:51:43 -0400

    messing with pistol animations

--- scenes/pistol.gd
+++ scenes/pistol.gd
@@ -1,4 +1,6 @@
 extends Node3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +11,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func shoot():
+	audio_stream_player_3d.play()
+	animation_player.play("fire")
+	
+	
+func aim():
+	animation_player.play("aim")
+	
+func un_aim():
+	animation_player.play("un_aim")
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,6 +2,9 @@ extends CharacterBody3D
 
 @onready var aim_ray: RayCast3D = $Cam/AimRay
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var weapon: Node3D = %Weapon.get_child(0)
+@onready var cam: Camera3D = %Cam
+
 const HOLE = preload("uid://tpbbr7m0pveq")
 
 const JUMP_VELOCITY = 4.5
@@ -10,6 +13,7 @@ var camera_speed:float = 10
 var camera_accel:float = 28
 var current_camera_speed:float = 0
 var current_camera_speed_x:float = 0
+var aimed:bool = false
 
 var settings: Settings
 
@@ -19,7 +23,17 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	
+	rotate_camera(delta)
+	
+	handle_buttons()
+	
 	var velocity_direction = Input.get_vector("m_left","m_right","m_fwd","m_back")
+	velocity = transform.basis * Vector3(velocity_direction.x, 0, velocity_direction.y) * move_speed
+	move_and_slide()	
+	
+	
+
+func rotate_camera(delta:float):
 	var y_rotation_direction = Input.get_axis("c_left","c_right")
 	var x_rotation_direction = Input.get_axis("c_up","c_down")
 	
@@ -37,22 +51,16 @@ func _physics_process(delta: float) -> void:
 		%Cam.rotate_x( deg_to_rad( current_camera_speed_x * delta))
 	else:
 		current_camera_speed_x = 0
-		
-	velocity = transform.basis * Vector3(velocity_direction.x, 0, velocity_direction.y) * move_speed
-		
-	move_and_slide()	
-	
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
 
 func shoot() -> void:
-	print("Shooting")
-	animation_player.play("fire_pistol")
+	#print("Shooting")
+	if weapon && weapon.has_method("shoot"):
+		weapon.shoot()
 	if aim_ray.is_colliding():
-		print("Hit")
+		#print("Hit")
 		var collider:Node3D = aim_ray.get_collider()
 		var shape = aim_ray.get_collider_shape()
-		print(collider)
+		#print(collider)
 		if collider.name == "Head": 		
 			var tar:Node3D = collider.get_parent().get_parent()
 			if tar.has_method("reset"):			
@@ -60,17 +68,29 @@ func shoot() -> void:
 		else:	
 			var collision_point = aim_ray.get_collision_point()
 			var collision_normal = aim_ray.get_collision_normal()
-			print(collision_point)
-			print(collision_normal)
+			#print(collision_point)
+			#print(collision_normal)
 			place_hole(collision_point,collision_normal)
+
+func handle_buttons():
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+		
+	if Input.is_action_just_pressed("aim"): 
+		if aimed:
+			un_aim()
+			aimed = false
+		else:
+			aim()
+			aimed = true
 
 func place_hole(position:Vector3, normal:Vector3) -> void:
 	
 	var hole:Node3D = HOLE.instantiate()
 	get_tree().root.add_child(hole)
 	hole.global_position = position
-	print("gto = ",hole.global_transform.origin)
-	print("look at = ",hole.global_transform.origin + normal)
+	#print("gto = ",hole.global_transform.origin)
+	#print("look at = ",hole.global_transform.origin + normal)
 	hole.look_at(hole.global_transform.origin + normal,Vector3.UP)
 	hole.rotate_object_local(Vector3(1,0,0),90)
 
@@ -82,3 +102,14 @@ func update_settings():
 
 func _on_settings_changed():
 	update_settings()
+	
+func aim():
+	if weapon.has_method("aim"):
+		weapon.aim()
+		%Cam.fov = 30
+		pass 
+		
+func un_aim():
+	if weapon.has_method("un_aim"):
+		weapon.un_aim()
+		%Cam.fov = 75
